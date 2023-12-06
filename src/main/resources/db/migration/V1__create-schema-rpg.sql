70c8cb4d677dc9752f553f36a45708af56198467
joaoagr1 <joao.rollo1@gmail.com>
2023-12-05 21:33:33 -0300

    dependencies_and_first_schema_migration

---
+++ src/main/resources/db/migration/V1__create-schema-rpg.sql
@@ -0,0 +1,59 @@
+-- Tabela Users
+CREATE TABLE Users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    login VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
+
+-- Tabela Characters
+CREATE TABLE Characters (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    alignment VARCHAR(255),
+    background VARCHAR(255),
+    features TEXT,
+    class VARCHAR(255),
+    race VARCHAR(255),
+    level INT,
+    character_name VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+) ENGINE=InnoDB;
+
+-- Tabela AttributePoints
+CREATE TABLE AttributePoints (
+    character_id INT PRIMARY KEY,
+    strength INT NOT NULL,
+    dexterity INT NOT NULL,
+    constitution INT NOT NULL,
+    intelligence INT NOT NULL,
+    wisdom INT NOT NULL,
+    charisma INT NOT NULL,
+    passive_perception INT NOT NULL,
+    FOREIGN KEY (character_id) REFERENCES Characters(id)
+) ENGINE=InnoDB;
+
+-- Tabela Spells
+CREATE TABLE Spells (
+    character_id INT,
+    user_id INT,
+    name VARCHAR(255),
+    description TEXT,
+    PRIMARY KEY (character_id, user_id),
+    FOREIGN KEY (character_id) REFERENCES Characters(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+) ENGINE=InnoDB;
+
+-- Tabela CharacterItems
+CREATE TABLE CharacterItems (
+    character_id INT,
+    user_id INT,
+    name VARCHAR(255),
+    description TEXT,
+    PRIMARY KEY (character_id, user_id),
+    FOREIGN KEY (character_id) REFERENCES Characters(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+) ENGINE=InnoDB;
---
+++ src/test/java/rpg/system/rpg/RpgApplicationTests.java
@@ -0,0 +1,13 @@
+package rpg.system.rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
