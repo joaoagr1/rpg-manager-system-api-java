aad0c4aa7449549d72f08aebd4b84affb1b8a6fb
João Antonio Garcia Rolo <joaorolo@usacucar.com.br>
2023-12-28 10:50:45 -0300

    creating_skills1

--- src/main/java/rpg/system/rpg/controller/SkillsController.java
+++ src/main/java/rpg/system/rpg/controller/SkillsController.java
@@ -1,4 +1,37 @@
 package rpg.system.rpg.controller;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rpg.system.rpg.model.domain.AttributePoints;
+import rpg.system.rpg.model.domain.Skills;
+import rpg.system.rpg.model.repositorys.AttributePointsRepository;
+import rpg.system.rpg.model.repositorys.SkillRepository;
+import rpg.system.rpg.model.services.RequestUpdateAttributes;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/skills")
 public class SkillsController {
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    //This endpoint returns the list of skills from a specific character...
+    @GetMapping("/{character_id}")
+    public ResponseEntity<List<Skills>> getAttributesByCharacterId(@PathVariable Long character_id) {
+        List<Skills> skills = skillRepository.getSkillsByCharacterId(character_id);
+        return ResponseEntity.ok(skills);
+    }
+
+   // @PutMapping("/{character_id}")
+    //public ResponseEntity<AttributePoints> updateAttributePointsByCharacterId(@PathVariable Long character_id, @RequestBody RequestUpdateAttributes dataAttribute) {
+      //  AttributePoints existingAttributes = skillRepository.findById(character_id)
+        //        .orElseThrow(() -> new EntityNotFoundException("Attributes not found"));
+        //existingAttributes.updatedataAttribute(dataAttribute);
+      //  AttributePoints updatedAttributes = skillRepository.save(existingAttributes);
+       // return ResponseEntity.ok(updatedAttributes);
+  //  }
 }
--- src/main/java/rpg/system/rpg/model/repositorys/SkillRepository.java
+++ src/main/java/rpg/system/rpg/model/repositorys/SkillRepository.java
@@ -3,12 +3,13 @@ package rpg.system.rpg.model.repositorys;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import rpg.system.rpg.model.domain.AttributePoints;
+import org.springframework.stereotype.Repository;
 import rpg.system.rpg.model.domain.Skills;
 
 import java.util.List;
 
-public interface SkillRepository extends JpaRepository<AttributePoints,Long> {
+@Repository
+public interface SkillRepository extends JpaRepository<Skills,Long> {
 
     @Query(value = "SELECT * FROM skills WHERE character_id = :character_id ", nativeQuery = true)
     List<Skills> getSkillsByCharacterId(@Param("character_id") Long character_id);
