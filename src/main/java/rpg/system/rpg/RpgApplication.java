5fda4891c4d0dedca421b0a1876ddd5015cf4bdc
João Antonio Garcia Rolo <joaorolo@usacucar.com.br>
2023-12-29 08:06:01 -0300

    29/12 8h2

--- src/main/java/rpg/system/rpg/RpgApplication.java
+++ src/main/java/rpg/system/rpg/RpgApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class    RpgApplication {
-
+//test
     public static void main(String[] args) {
         SpringApplication.run(RpgApplication.class, args);
     }
