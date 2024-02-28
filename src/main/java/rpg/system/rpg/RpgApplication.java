52c960d2a489460ea1f43cbf2a06afe4d18f6688
João Antonio Garcia Rolo <joaorolo@usacucar.com.br>
2024-02-28 14:26:40 -0300

    funcionando parcialmente

--- src/main/java/rpg/system/rpg/RpgApplication.java
+++ src/main/java/rpg/system/rpg/RpgApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //http://localhost:8080/swagger-ui/index.html#/
 @SpringBootApplication
 public class    RpgApplication {
+    //// spring.datasource.url=jdbc:mysql://rpgfinal.cdsgu4wqqnmf.sa-east-1.rds.amazonaws.com:3306/rpgfinal
     public static void main(String[] args) {
         SpringApplication.run(RpgApplication.class, args);
     }
--- src/test/java/rpg/system/rpg/RpgApplicationTests.java
+++ src/test/java/rpg/system/rpg/RpgApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RpgApplicationTests {
+	//spring.flyway.schemas=rpgdata
 
 	@Test
 	void contextLoads() {
